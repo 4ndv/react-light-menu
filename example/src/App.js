f38a1db1afd6a62c5009d00447e77cfb2ea63bc9
Andrey Viktorov <andreyviktorov@outlook.com>
2020-03-12 18:48:27 +0700

    Example changes

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import ExampleComponent from 'react-router-light-menu'
+import LightMenu from 'react-router-light-menu'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <LightMenu items={[ { hello: 'world' } ]} />
       </div>
     )
   }
