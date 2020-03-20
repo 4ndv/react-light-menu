d770e5ffebc9027b8d8efaa8fb43be3d8d39f778
Andrey Viktorov <andreyviktorov@outlook.com>
2020-03-20 20:17:21 +0700

    Updates

---
+++ precache-manifest.4f6ddad86140e03c5b91db59c6d26f16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f8cf7b3c0c79ad6db57182cdc2f890",
+    "url": "/react-light-menu/index.html"
+  },
+  {
+    "revision": "586380dd3f693d96bada",
+    "url": "/react-light-menu/static/css/main.941786f9.chunk.css"
+  },
+  {
+    "revision": "f48e6aafd1f8af38fdff",
+    "url": "/react-light-menu/static/js/2.37bfc14a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-light-menu/static/js/2.37bfc14a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586380dd3f693d96bada",
+    "url": "/react-light-menu/static/js/main.56bd175c.chunk.js"
+  },
+  {
+    "revision": "3fc6ff4c81cd6a523bee927348ab17ad",
+    "url": "/react-light-menu/static/js/main.56bd175c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f6a6834332de622778",
+    "url": "/react-light-menu/static/js/runtime-main.1af86a35.js"
+  }
+]);
